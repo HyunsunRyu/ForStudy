5bbf4f3f7eb6f9acff5d645429ce7c66413dceb9
HyunsunRyu <lovedangyou@gmail.com>
2019-02-26 23:07:07 +0900

    에디터 창 작업 진행 중 - 작업 중 잘 시간이 되어서 일단 커밋

---
+++ Assets/CustomEditor/Scritps/Editor/CustomWindow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Study
+{
+    public class CustomWindow : EditorWindow
+    {
+        private Vector2 scrollPos;
+
+        private List<Data> dataList;
+        private List<DisplayData> displayDataList;
+
+        [MenuItem("Study/Custom Editor Window")]
+        private static void Init()
+        {
+            CustomWindow window = (CustomWindow)EditorWindow.GetWindow(typeof(CustomWindow));
+            window.titleContent = new GUIContent("Custom Window");
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            if (dataList == null || displayDataList == null)
+            {
+                dataList = new List<Data>();
+                displayDataList = new List<DisplayData>();
+            }
+            dataList.Clear();
+            displayDataList.Clear();
+
+            string[] guides = AssetDatabase.FindAssets("t:CustomWindow", new string[] { "Assets/CustomEditor/Data" });
+            foreach (string guid in guides)
+            {
+                Data data = AssetDatabase.LoadAssetAtPath<Data>(guid);
+                if (data != null)
+                {
+                    dataList.Add(data);
+                    displayDataList.Add(new DisplayData());
+                }
+            }
+        }
+
+        private void OnGUI()
+        {
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Height(300f));
+
+            foreach (Data data in dataList)
+            {
+                
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
---
+++ Assets/CustomEditor/Scritps/Editor/Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Study
+{
+    [CreateAssetMenu(menuName = "Create Data/Custom Data")]
+    public class Data : ScriptableObject
+    {
+        public List<int> values;
+    }
+}
